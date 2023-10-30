2d048da9fd1d9ad493c415ef105f75cbd88adeaa
keshav0306 <iamkeshav06@gmail.com>
2023-10-30 15:57:49 +0530

    added move1D and move2D functions

--- infermnist.py
+++ infermnist.py
@@ -10,7 +10,8 @@ import torch.nn.functional as F
 import copy
 import matplotlib.pyplot as plt
 from models.cnn import CNN
-from visfuncs  import interpolate
+from visfuncs  import interpolate, move1D, move2D
+import numpy as np
 
 def give_model(path):
     model_init = CNN()
@@ -19,11 +20,30 @@ def give_model(path):
 
     return model_init
 
-def plot_loss_acc(model1, model2, criterion, dataloader, device):
-    loss, accs = interpolate(dataloader, criterion, model1, model2, device, log=True)
-    plt.plot(loss)
+def plot_loss_acc_inter(model1, model2, criterion, dataloader, device):
+    stepsize = 20
+    loss, accs, x = interpolate(dataloader, criterion, model1, model2, stepsize, device, log=True)
+    plt.plot(x, loss)
     plt.show()
-    plt.plot(accs)
+    plt.plot(x, accs)
+    plt.show()
+
+def plot_loss_acc_move1D(model, dirn, criterion, dataloader, device):
+    stepsize = 20
+    loss, accs, x = move1D(dataloader, criterion, model, dirn, stepsize, device, log=True)
+    plt.plot(x, loss)
+    plt.show()
+    plt.plot(x, accs)
+    plt.show()
+
+def plot_loss_acc_move2D(model, dirn1, dirn2, criterion, dataloader, device):
+    stepsize = 10
+    loss, accs, x, y = move2D(dataloader, criterion, model, dirn1, dirn2, stepsize, stepsize, device, log=True)
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(x, y, loss)
+    plt.show()
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(x, y, accs)
     plt.show()
 
 # Define data transforms
@@ -45,4 +65,21 @@ model2 = give_model('weights/m2.pt')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 criterion = nn.CrossEntropyLoss()
 
-plot_loss_acc(model1, model2, criterion, test_loader, device)
+# plot_loss_acc_inter(model1, model2, criterion, test_loader, device)
+
+# dirn = CNN()
+# for name, param in dirn.named_parameters():
+#     param.data = torch.randn_like(param.data)
+
+# plot_loss_acc_move1D(model1, dirn, criterion, test_loader, device)
+
+dirn1 = CNN()
+for name, param in dirn1.named_parameters():
+    param.data = torch.randn_like(param.data)
+
+
+dirn2 = CNN()
+for name, param in dirn2.named_parameters():
+    param.data = torch.randn_like(param.data)
+
+plot_loss_acc_move2D(model1, dirn1, dirn2, criterion, test_loader, device)
--- visfuncs.py
+++ visfuncs.py
@@ -11,6 +11,14 @@ import copy
 
 @torch.no_grad()
 def give_loss_acc(dataloader, model, criterion, device):
+    '''
+    dataloader is a torch.Dataloader instance
+    criterion is the loss function
+    model is the model architecture with its parameters
+    device is the device on which this code is running on
+
+    returns the loss and accuracy 
+    '''
     loss = 0
     num = 0
     corr = 0
@@ -26,8 +34,17 @@ def give_loss_acc(dataloader, model, criterion, device):
     return loss, acc
 
 
-def interpolate(dataloader, criterion, model1, model2, device, log=False):
-    stepsize = 20
+def interpolate(dataloader, criterion, model1, model2, stepsize, device, log=False):
+    '''
+    dataloader is a torch.Dataloader instance
+    criterion is the loss function
+    model1 is the model architecture with its parameters
+    model2 is the model architecture with its parameters
+    device is the device on which this code is running on
+    log is whether to print the loss and acc at each step
+
+    returns the loss and accuracy lists
+    '''
     alpha = torch.linspace(0, 1, stepsize)
     model = copy.deepcopy(model1)
     losses = []
@@ -43,4 +60,75 @@ def interpolate(dataloader, criterion, model1, model2, device, log=False):
         losses.append(loss)
         accs.append(acc)
     
-    return losses, accs
+    return losses, accs, alpha.numpy()
+
+def move1D(dataloader, criterion, model1, dirn, stepsize, device, log=False):
+    '''
+    dataloader is a torch.Dataloader instance
+    criterion is the loss function
+    model1 is the model architecture with its parameters
+    dirn is the same model architecture with weights as the dirn vectors for the corresponding weight
+    device is the device on which this code is running on
+    log is whether to print the loss and acc at each step
+
+    returns the loss and accuracy lists
+    '''
+
+    alpha = torch.linspace(-1, 1, stepsize)
+    model = copy.deepcopy(model1)
+    losses = []
+    accs = []
+    for a in alpha:
+        for i, j, k in zip(model1.parameters(), dirn.parameters(), model.parameters()):
+            k.data = i.data + a*j.data
+
+        loss, acc = give_loss_acc(dataloader, model, criterion, device)
+        if(log):
+            print(loss)
+            print(acc)
+        losses.append(loss)
+        accs.append(acc)
+    
+    return losses, accs, alpha.numpy()
+
+def move2D(dataloader, criterion, model1, dirn1, dirn2, stepsize1, stepsize2, device, log=False):
+    '''
+    dataloader is a torch.Dataloader instance
+    criterion is the loss function
+    model1 is the model architecture with its parameters
+    dirn1 is the same model architecture with weights as the dirn vectors for the corresponding weight
+    dirn2 is the same model architecture with weights as the dirn vectors for the corresponding weight
+    stepsize1 is the number of steps to be taken along x
+    stepsize2 is the number of steps to be taken along y
+    device is the device on which this code is running on
+    log is whether to print the loss and acc at each step
+
+    returns the loss and accuracy lists
+    '''
+
+    alpha = torch.linspace(-1, 1, stepsize1)
+    beta = torch.linspace(-1, 1, stepsize2)
+
+    model = copy.deepcopy(model1)
+
+    losses = []
+    accs = []
+
+    for a in alpha:
+        for b in beta:
+            for i, d1, d2, k in zip(model1.parameters(), dirn1.parameters(), dirn2.parameters(), model.parameters()):
+                k.data = i.data + a*d1.data + b*d2.data
+
+            loss, acc = give_loss_acc(dataloader, model, criterion, device)
+            if(log):
+                print(loss)
+                print(acc)
+            losses.append(loss)
+            accs.append(acc)
+    
+    losses = torch.tensor(losses).reshape(stepsize1, stepsize2).numpy()
+    accs = torch.tensor(accs).reshape(stepsize1, stepsize2).numpy()
+    x, y = torch.meshgrid(alpha, beta)
+    print(x)
+    print(y)
+    return losses, accs, x.numpy(), y.numpy()
