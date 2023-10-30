55cecbf8d20198db9bd6e6bc5b6fd38d08cc1f17
Soham Korade <sohamkorade.jnvkk@gmail.com>
2023-10-31 01:53:51 +0530

    sbatch files

---
+++ sbatch-resnet-500-32-noshort.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -A neuro 
+#SBATCH -n 10
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=4-00:00:00
+#SBATCH --output=darknet_file.txt
+#SBATCH --partition=ihub
+module load u18/cuda/10.2
+module load u18/cudnn/7.6.5-cuda-10.2
+
+python3 resnet_train_cifar10.py --epochs 500 --batch_size 32
+
+# echo "Hello World"
