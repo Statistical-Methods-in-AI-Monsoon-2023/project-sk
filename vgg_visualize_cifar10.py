bdf40c2a0bf7455789acb639c70215be4c7770b8
Soham Korade <sohamkorade.jnvkk@gmail.com>
2023-10-31 01:03:33 +0530

    visualize vgg

---
+++ vgg_visualize_cifar10.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torchvision.datasets import MNIST
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import torchvision.models as models
+import torch.nn.functional as F
+import copy
+import matplotlib.pyplot as plt
+from models.vgg import VGG, vgg_cfgs
+from visfuncs  import interpolate, move1D, move2D
+from data import load_cifar10
+import numpy as np
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--model_name', type=str, default='resm1', help='Name of the model')
+parser.add_argument('--weight_path', type=str, default='resm1.pt', help='Path to the model')
+parser.add_argument('--num_layers', type=int, default=11, help='Number of layers in the VGG model')
+
+args = parser.parse_args()
+
+
+def give_model(path, device):
+    model_init = torch.load(path, map_location=device)
+    model_init.eval()
+
+    return model_init
+
+def plot_loss_acc_inter(model1, model2, criterion, dataloader, device):
+    stepsize = 20
+    loss, accs, x = interpolate(dataloader, criterion, model1, model2, stepsize, device, log=True)
+    np.save("loss.npy", loss)
+    np.save("accs.npy", accs)
+    plt.plot(x, loss)
+    plt.show()
+    plt.plot(x, accs)
+    plt.show()
+
+def plot_loss_acc_move1D(model, dirn, criterion, dataloader, device):
+    stepsize = 20
+    loss, accs, x = move1D(dataloader, criterion, model, dirn, stepsize, device, log=True)
+    plt.plot(x, loss)
+    plt.show()
+    plt.plot(x, accs)
+    plt.show()
+
+def plot_loss_acc_move2D(model, dirn1, dirn2, criterion, dataloader, device):
+    stepsize = 20
+    loss, accs, x, y = move2D(dataloader, criterion, model, dirn1, dirn2, stepsize, stepsize, device, log=True)
+    np.save('2d'+args.model_name+'.npy', np.array([[loss], [accs], [x], [y]]))
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(x, y, loss)
+    plt.show()
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(x, y, accs)
+    plt.show()
+
+train_loader, test_loader = load_cifar10(128, 2)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model1 = give_model('weights/'+args.weight_path, device)
+# model2 = give_model('weights/resm2.pt', device)
+
+# Uncomment for printing the parameters of the model
+
+# for name, param in model2.named_parameters():
+    # print(name)
+    # print(param.shape)
+
+criterion = nn.CrossEntropyLoss()
+
+#plot_loss_acc_inter(model1, model2, criterion, train_loader, device)
+
+# dirn = ResNet()
+# for name, param in dirn.named_parameters():
+#     param.data = torch.randn_like(param.data)
+
+# plot_loss_acc_move1D(model1, dirn, criterion, test_loader, device)
+
+vgg_num_layers = str(args.num_layers)
+vgg_id = "vgg" + vgg_num_layers
+if vgg_id in vgg_cfgs:
+    pass
+else:
+    print("Invalid VGG Configuration")
+    exit()
+
+dirn1 = VGG(vgg_cfgs[vgg_id], True)
+dirn1.to(device)
+for param, m_param in zip(dirn1.parameters(), model1.parameters()):
+     param.data = torch.randn_like(param.data)
+     param.data = param.data / torch.linalg.norm(param.data)
+     param.data *= m_param
+
+dirn2 = VGG(vgg_cfgs[vgg_id], True)
+dirn2.to(device)
+for param, m_param in zip(dirn2.parameters(), model1.parameters()):
+    param.data = torch.randn_like(param.data)
+    param.data = param.data / torch.linalg.norm(param.data)
+    param.data *= m_param 
+
+plot_loss_acc_move2D(model1, dirn1, dirn2, criterion, train_loader, device)
