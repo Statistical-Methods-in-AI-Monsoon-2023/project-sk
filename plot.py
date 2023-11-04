aadbf4b17289eb75c34151ea549216587cdaf22c
Soham Korade <sohamkorade.jnvkk@gmail.com>
2023-11-04 14:25:19 +0530

    convert ndarray to json for web plot

--- plot.py
+++ plot.py
@@ -10,7 +10,10 @@ args = parser.parse_args()
 
 # loss = np.load('loss.npy', allow_pickle=True)
 # accs = np.load('accs.npy', allow_pickle=True)
-loss, accs, x, y = np.load(args.file, allow_pickle=True)
+array = np.load(args.file, allow_pickle=True)
+loss, accs, x, y = array
+print(array.shape)
+print(loss.shape, accs.shape, x.shape, y.shape)
 loss = loss[0]
 x = x[0]
 y = y[0]
@@ -19,6 +22,21 @@ accs = accs[0]
 # replace nan with 0
 loss = np.nan_to_num(loss)
 accs = np.nan_to_num(accs)
+
+# convert to json as array of {x,y,z}
+import json
+
+loss_list = []
+for i in range(len(x)):
+    row=[]
+    for j in range(len(y)):
+        row.append({
+            'x': float(x[i, 0])/10,
+            'y': float(y[j, 0])/10,
+            'z': float(loss[i][j])+1
+        })
+    loss_list.append(row)
+json.dump(loss_list, open('viz/loss.json', 'w'), indent=2)
 
 # take filename from path
 model_unique_id = args.file.split('/')[-1].split('.')[0]
@@ -30,13 +48,13 @@ os.makedirs('results/plots/', exist_ok=True)
 ax = plt.axes(projection='3d')
 ax.plot_surface(x, y, loss)
 if args.show:
-	plt.show()
+    plt.show()
 else:
-	plt.savefig(f'results/plots/loss-{model_unique_id}.png')
+    plt.savefig(f'results/plots/loss-{model_unique_id}.png')
 
 ax = plt.axes(projection='3d')
 ax.plot_surface(x, y, accs)
 if args.show:
-	plt.show()
+    plt.show()
 else:
-	plt.savefig(f'results/plots/acc-{model_unique_id}.png')
+    plt.savefig(f'results/plots/acc-{model_unique_id}.png')
