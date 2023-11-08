63146eb6edca765f5bee736bc665d1c21572847b
Soham Korade <sohamkorade.jnvkk@gmail.com>
2023-11-08 18:22:34 +0530

    plot json export

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Plot loss and accuracy')
 parser.add_argument('--file', type=str)
 parser.add_argument('--show', action='store_true')
+parser.add_argument('--json', action='store_true')
 args = parser.parse_args()
 
 # loss = np.load('loss.npy', allow_pickle=True)
@@ -20,23 +21,27 @@ y = y[0]
 accs = accs[0]
 
 # replace nan with 0
-loss = np.nan_to_num(loss)
-accs = np.nan_to_num(accs)
+loss = np.nan_to_num(loss,posinf=0,neginf=0)
+accs = np.nan_to_num(accs,posinf=0,neginf=0)
 
-# convert to json as array of {x,y,z}
-import json
+# log scale
+loss = np.log(loss)
+accs = np.log(accs)
 
-loss_list = []
-for i in range(len(x)):
-    row=[]
-    for j in range(len(y)):
-        row.append({
-            'x': float(x[i, 0])/10,
-            'y': float(y[j, 0])/10,
-            'z': float(loss[i][j])+1
-        })
-    loss_list.append(row)
-json.dump(loss_list, open('viz/loss.json', 'w'), indent=2)
+# print(loss.min(), loss.max())
+
+def to_xyz_array(arr):
+    array = []
+    for i in range(len(x)):
+        row=[]
+        for j in range(len(y)):
+            row.append({
+                'x': float(x[i,0]),
+                'y': float(y[0,j]),
+                'z': float(arr[i][j])
+            })
+        array.append(row)
+    return array
 
 # take filename from path
 model_unique_id = args.file.split('/')[-1].split('.')[0]
@@ -44,6 +49,14 @@ model_unique_id = args.file.split('/')[-1].split('.')[0]
 # mkdir results/plots/
 import os
 os.makedirs('results/plots/', exist_ok=True)
+os.makedirs('results/plot_json/', exist_ok=True)
+
+if args.json:
+    import json
+    json.dump({
+        'loss': to_xyz_array(loss),
+        'acc': to_xyz_array(accs)
+    }, open(f'results/plot_json/{model_unique_id}.json', 'w'), indent=2)
 
 ax = plt.axes(projection='3d')
 ax.plot_surface(x, y, loss)
