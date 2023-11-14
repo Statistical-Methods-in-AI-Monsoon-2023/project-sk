2c8d0e8f5b1f19dcc6877ea96f0e766e4a8d03b9
keshav0306 <iamkeshav06@gmail.com>
2023-11-14 22:20:34 +0530

    added idd

---
+++ idd_train.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from models.resnetIDD import ResNet56IDD
+from data import load_idd
+import os
+from torch.distributed import init_process_group, destroy_process_group
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.multiprocessing as mp
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Train a model on IDD')
+parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train for')
+parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
+parser.add_argument('--save_every', type=int, default=-1, help='Save every save_every iterations')
+parser.add_argument('--model', type=str, help='Name of the model',required=True)
+parser.add_argument('--data', type=str, help='Path to the IDD images', default="../train_idd/")
+
+
+def setup(rank, args):
+    # Ininitalizes the process_group and makes this process a part of that group. 
+    # MASTER_ADDR and MASTER_PORT are required for the processes to communicate to the master node
+
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '3003'
+    init_process_group('nccl', rank=rank, world_size=args.world_size)
+
+def train(rank, args):
+
+    setup(rank, args)
+
+    train_loader, test_loader = load_idd(args.data, int(args.batch_size), 2, distributed=True)
+
+    # Create the ResNet model and optimizer
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    model = ResNet56IDD(args.model)
+    model_unique_id = f"{model.get_unique_id()}-{args.batch_size}-{args.epochs}epochs"
+    # Create a DDP instance
+    model.to(rank)
+    model = DDP(model, device_ids=[rank])
+    optimizer = optim.Adam(model.parameters(), lr=0.1)
+    # Training loop
+    num_epochs = args.epochs
+    count = 0
+    t1 = time.time()
+
+    if(args.save_every != -1):
+        os.makedirs(f'weights/{model_unique_id}', exist_ok=True)
+
+    for epoch in range(num_epochs):
+        model.train()
+        count = 0 
+        for batch_idx, (data, target) in enumerate(train_loader):
+            count += 1
+            optimizer.zero_grad()
+            data = data.to(rank)
+            target = target.to(rank)
+            output = model(data)
+            loss = F.cross_entropy(output, target)
+            loss.backward()
+            optimizer.step()
+        
+            if batch_idx % 100 == 0:
+                print(f'GPU {rank} : Epoch {epoch}, Batch {batch_idx}, Loss: {loss.item()}')
+        
+        if(rank == 0):
+            if(epoch % args.save_every == 0 and args.save_every != -1):
+                torch.save(model.module, f"weights/{model_unique_id}/{epoch}.pt")
+    if(rank == 0):
+        torch.save(model.module, f"weights/{model_unique_id}.pt")
+    
+    # Test the model
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data = data.to(rank)
+            target = target.to(rank)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum()
+
+    torch.distributed.all_reduce(tensor=correct, op=torch.distributed.ReduceOp.SUM)
+    test_loss /= len(test_loader.dataset)
+    accuracy = 100.0 * correct / len(test_loader.dataset)
+    print(f'Test set: Average loss: {test_loss:.4f}, Accuracy: {accuracy}%')
+    destroy_process_group()
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    world_size = torch.cuda.device_count()
+    args.world_size = world_size
+    print(world_size)
+    print(args)
+    
+    # Creates (world_size) number of processes
+    mp.spawn(train, args=[args], nprocs=world_size)
---
+++ models/resnetIDD.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1   = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2   = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        # out = F.relu(self.conv1(x))
+        # out = (self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+    
+class BasicBlockNoShort(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlockNoShort, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1   = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2   = nn.BatchNorm2d(planes)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        # out = F.relu(self.conv1(x))
+        # out = (self.conv2(out))
+
+        out = F.relu(out)
+        return out
+
+
+class ResNet56IDD(nn.Module):
+    def __init__(self, margs="noshort", num_blocks=[9,9,9], num_classes=10):
+        super(ResNet56IDD, self).__init__()
+        # Img of shape (B, 3, H, W) becomes (B, 64, H/32, W/32)
+        
+        self.in_planes = 16
+
+        self.block_type = margs
+        if margs == "short":
+            block = BasicBlock
+        elif margs == "noshort":
+            block = BasicBlockNoShort
+        else:
+            raise ValueError("block_type must be one of 'short' or 'noshort'")
+
+        self.conv1  = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1    = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+
+        # Heatmap and Confidence scores
+        self.projConv = nn.Conv2d(64, 2, kernel_size=1, stride=1, padding=1, bias=False)
+
+        # self.linear = nn.Linear(64*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        # out = F.relu(self.conv1(x))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = F.avg_pool2d(out, 8)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+    
+    def get_unique_id(self):
+        return f"resnet-{self.block_type}"
