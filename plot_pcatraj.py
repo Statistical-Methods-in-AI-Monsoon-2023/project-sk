6d643c839f83baf249abe03d08b406d4d6acb47a
Soham Korade <sohamkorade.jnvkk@gmail.com>
2023-11-21 01:12:11 +0530

    pca contour plot

--- plot_pcatraj.py
+++ plot_pcatraj.py
@@ -22,9 +22,12 @@ from models import give_model
 
 parser = argparse.ArgumentParser(description='Plot loss and accuracy')
 parser.add_argument('--folderpath', type=str)
+parser.add_argument('--range', type=int, default=20)
 parser.add_argument('--show', action='store_true')
 parser.add_argument('--json', action='store_true')
 parser.add_argument('--model', type=str, help='Name of the model',required=True)
+
+finalmodel = None
 
 @torch.no_grad()
 def give_loss_acc(dataloader, model, criterion, device):
@@ -70,19 +73,15 @@ def vis(rank, model, dirn1, dirn2, criterion, steps, indices, output):
         output[indices[s], 1] = acc
 
 
-def plot_pca_traj(args):
-
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    nprocs = torch.cuda.device_count()
-    workers = []
-    criterion = nn.CrossEntropyLoss()
-
+def get_eigvecs(args):
     models = []
     for file in os.listdir(args.folderpath):
         if file.endswith(".pt"):
             model = torch.load(os.path.join(args.folderpath, file), map_location=device)
             model.eval()
             models.append(model)
+    global finalmodel
+    finalmodel = models[-1]
     concats = []
     for i in range(len(models)):
         model = models[i]
@@ -100,28 +99,23 @@ def plot_pca_traj(args):
     pca = PCA(n_components=2)
     pca.fit(diff_concats)
     eigvecs = pca.components_
-
     print("Variances: ", pca.explained_variance_ratio_)
-
-    # print(eigvecs)
-
     diff_concats = torch.tensor(diff_concats).to(device)
     eigvecs = torch.tensor(eigvecs).to(device)
     eigvecs = eigvecs / torch.linalg.norm(eigvecs, dim=1).reshape(-1, 1)
+    return diff_concats, eigvecs
+
+def give_proj(diff_concats, eigvecs):
     x_vals, y_vals = eigvecs @ diff_concats.T
     x_vals = x_vals.detach().cpu().numpy()
     y_vals = y_vals.detach().cpu().numpy()
-    # xlist, ylist = [], []
-    # for i in range(len(models) -1 ):
-    #     xlist.append((torch.dot(eigvecs[0], diff_concats[i])/torch.norm(eigvecs[0])).detach().cpu().item())
-    #     ylist.append((torch.dot(eigvecs[1], diff_concats[i])/torch.norm(eigvecs[1])).detach().cpu().item())
 
-    # plot trajectory
+    return x_vals, y_vals
 
+def plot_trajectory(x_vals,y_vals):
     print(x_vals.shape, y_vals.shape)
     print(x_vals, y_vals)
 
-    plt.figure()
     plt.scatter(x_vals, y_vals)
     plt.xlabel("PC1")
     plt.ylabel("PC2")
@@ -132,68 +126,97 @@ def plot_pca_traj(args):
     else:
         plt.savefig(f"results/{args.model}-pca.png")
 
-    # if "noshort" in args.weights:
-    #     block = BasicBlockNoShort
-    # else:
-    #     block = BasicBlock
-
-    # dirn1 = ResNet(block, [9,9,9])
-    # # dirn1.to(device)
-    # for param, m_param in zip(dirn1.parameters(), model.parameters()):
-    #     if(len(m_param.shape) == 1):
-    #         param = m_param
-    #         continue
-    #     param.data = torch.randn_like(param.data)
-    #     param.data = param.data / torch.linalg.norm(param.data)
-    #     param.data *= m_param
-
-    # dirn2 = ResNet(block, [9,9,9])
-    # # dirn2.to(device)
-    # for param, m_param in zip(dirn2.parameters(), model.parameters()):
-    #     if(len(m_param.shape) == 1):
-    #         param = m_param
-    #         continue
-    #     param.data = torch.randn_like(param.data)
-    #     param.data = param.data / torch.linalg.norm(param.data)
-    #     param.data *= m_param 
-
-    # alpha = torch.linspace(-1, 1, 20)
-    # beta = torch.linspace(-1, 1, 20)
-    # mesh_x, mesh_y = torch.meshgrid(alpha, beta)
-    # mesh = torch.cat([mesh_x.unsqueeze(0), mesh_y.unsqueeze(0)], 0).permute(1, 2, 0).reshape(-1, 2)
     
-    # num_per_proc = mesh.shape[0] // nprocs
-    # steps = [mesh[i*num_per_proc:(i+1)*num_per_proc, :] for i in range(nprocs-1)]
-    # steps.append(mesh[(nprocs-1)*(num_per_proc):, :])
-    # indices = [torch.arange(i*num_per_proc, (i+1)*num_per_proc) for i in range(nprocs-1)]
-    # indices.append(torch.arange((nprocs-1)*num_per_proc, mesh.shape[0]))
 
-    # # Loss and Accuracy Values
-    # output = torch.zeros(mesh.shape[0], 2)
-    # output.share_memory_()
-
-    # mp.set_start_method('spawn', force=True)
-
-    # for i in range(nprocs):
-    #     p = mp.Process(target=vis, args=[i, model, dirn1, dirn2, criterion, steps[i], indices[i], output])
-    #     p.start()
-    #     workers.append(p)
-
-    # for i in range(nprocs):
-    #     workers[i].join()
-
-    # loss = output[:, 0].reshape(mesh_x.shape)
-    # acc = output[:, 1].reshape(mesh_x.shape)
-
-    # plt.figure()
-    # plt.contourf(mesh_x, mesh_y, loss, 20)
-    # plt.colorbar()
-    # plt.xlabel("alpha")
-    # plt.ylabel("beta")
-    # plt.title("Loss Contour")
-    # plt.savefig("loss_contour.png")
-            
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    plot_pca_traj(args)
+    filename = os.path.basename(args.folderpath)
+    npy_data = f"results/plot_npy/{filename}-pcatraj-{args.range}.npy"
+    
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    nprocs = torch.cuda.device_count()
+    workers = []
+    criterion = nn.CrossEntropyLoss()
+    diff_concats, eigvecs=get_eigvecs(args)
+    x_vals, y_vals = give_proj(diff_concats, eigvecs)
+    plot_trajectory(x_vals, y_vals)
+
+    if not os.path.exists(npy_data):
+        # calculate step size
+        x_min, x_max = np.min(x_vals), np.max(x_vals)
+        y_min, y_max = np.min(y_vals), np.max(y_vals)
+        x_std = np.std(x_vals)
+        y_std = np.std(y_vals)
+        x_min-=x_std
+        x_max+=x_std
+        y_min-=y_std
+        y_max+=y_std
+        print("x_min, x_max, y_min, y_max: ", x_min, x_max, y_min, y_max)
+
+        v_ten1 = eigvecs[0]
+        v_ten2 = eigvecs[1]
+
+        model = finalmodel
+        dirn1 = give_model(args)
+        # dirn1.to(device)
+        total_len = 0
+        for param, m_param in zip(dirn1.parameters(), model.parameters()):
+            param_len = param.reshape(-1).shape[0]
+            param.data = v_ten1[total_len:total_len+param_len].reshape(param.shape)
+            total_len += param_len
+
+        dirn2 = give_model(args)
+        total_len = 0
+        for param, m_param in zip(dirn2.parameters(), model.parameters()):
+            param_len = param.reshape(-1).shape[0]
+            param.data = v_ten2[total_len:total_len+param_len].reshape(param.shape)
+            total_len += param_len
+
+        alpha = torch.linspace(x_min, x_max, args.range)
+        beta = torch.linspace(y_min, y_max, args.range)
+        mesh_x, mesh_y = torch.meshgrid(alpha, beta)
+        mesh = torch.cat([mesh_x.unsqueeze(0), mesh_y.unsqueeze(0)], 0).permute(1, 2, 0).reshape(-1, 2)
+        
+        num_per_proc = mesh.shape[0] // nprocs
+        steps = [mesh[i*num_per_proc:(i+1)*num_per_proc, :] for i in range(nprocs-1)]
+        steps.append(mesh[(nprocs-1)*(num_per_proc):, :])
+        indices = [torch.arange(i*num_per_proc, (i+1)*num_per_proc) for i in range(nprocs-1)]
+        indices.append(torch.arange((nprocs-1)*num_per_proc, mesh.shape[0]))
+
+        # Loss and Accuracy Values
+        output = torch.zeros(mesh.shape[0], 2)
+        output.share_memory_()
+
+        mp.set_start_method('spawn', force=True)
+
+        for i in range(nprocs):
+            p = mp.Process(target=vis, args=[i, model, dirn1, dirn2, criterion, steps[i], indices[i], output])
+            p.start()
+            workers.append(p)
+
+        for i in range(nprocs):
+            workers[i].join()
+        np.save(npy_data, np.array([[output[..., 0]], [output[..., 1]], [mesh_x.numpy()], [mesh_y.numpy()]]))
+    else:
+        output = np.load(npy_data, allow_pickle=True)
+        loss,acc,mesh_x,mesh_y = output
+        loss = loss[0]
+        acc = acc[0]
+        mesh_x = mesh_x[0]
+        mesh_y = mesh_y[0]
+
+    # print(loss.shape, acc.shape, mesh_x.shape, mesh_y.shape)
+
+    # contour plot
+    plt.contour(mesh_x, mesh_y, loss, cmap='RdGy',
+                levels=20, vmin=loss.min(), vmax=loss.max())
+    plt.colorbar()
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.title("Loss Contour Plot")
+    if args.show:
+        plt.show()
+    else:
+        plt.savefig(f"results/{args.model}-pca-contour.png")
+
