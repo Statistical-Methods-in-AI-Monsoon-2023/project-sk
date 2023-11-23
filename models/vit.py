9aa96934733438e2d4d2dd624ca74992cd61b81b
keshav0306 <iamkeshav06@gmail.com>
2023-11-23 07:27:41 +0530

    removed bug vit

--- models/vit.py
+++ models/vit.py
@@ -73,8 +73,8 @@ class VIT(nn.Module):
         patches = patches.permute(0, 2, 3, 1, 4, 5) # (B, Nx, Ny, C, P, P)
         patches = patches.reshape(B, self.num_patches, -1)
         patch_embed = self.patch_embedding(patches) + self.pos_embeddings # (B, N, D)
-        patch_embed = torch.column_stack([patch_embed, self.cls_token.reshape(1, 1, -1)], dim = 1)
-        encoded = self.encoder(patch_embed) # (B, N, D)
+        patch_embed = torch.cat([patch_embed, torch.tile(self.cls_token, (B, 1, 1))], dim = 1) # (B, N+1, D)
+        encoded = self.encoder(patch_embed) # (B, N+1, D)
         encoded = encoded[:, -1] # (B, D)
         out_logits = self.final_proj(encoded) # (B, num_classes)
         
