d53b57c4870b6a3bee20e14d05aa5a00a90b40ec
keshav0306 <iamkeshav06@gmail.com>
2023-11-28 14:08:14 +0530

    res working

--- models/resnet.py
+++ models/resnet.py
@@ -96,7 +96,7 @@ class ResNet(nn.Module):
         elif(resnet_size == 110):
             num_blocks = [18, 18, 18]
 
-        self.avg_pool = nn.AvgPool2d(self.final_avg_pool)
+        #self.avg_pool = nn.AvgPool2d(self.final_avg_pool)
         if self.block_type == "short":
             print("short")
             block = BasicBlock
@@ -126,8 +126,8 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = self.avg_pool(out)
-        #out = F.avg_pool2d(out, 8)
+        #out = self.avg_pool(out)
+        out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
