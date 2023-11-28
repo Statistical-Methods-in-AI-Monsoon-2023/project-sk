1978d5217af554699f30c06fd1d162a40252e52f
Soham Korade <sohamkorade.jnvkk@gmail.com>
2023-11-28 14:30:09 +0530

    make sbatch update

--- make_sbatch.py
+++ make_sbatch.py
@@ -3,15 +3,19 @@ sbatch_file = open("sbatch/sbatch.sh", "r").read()
 
 import argparse
 import os
+from collections import OrderedDict
 
 from models import gen_unique_id
 
 parser = argparse.ArgumentParser(description="Make SBATCH v1.0")
 parser.add_argument("--account", "-A", "-a", type=str, default="neuro")
+parser.add_argument("--single_file", "-s", action="store_true")
 args = parser.parse_args()
 
-hyperparameters = {
-    "model": [
+hyperparameters = OrderedDict()
+
+
+hyperparameters["model"] = [
         "resnet-noshort",
         "resnet-short",
         "vgg-11-bn",
@@ -22,28 +26,27 @@ hyperparameters = {
         # "vgg-13",
         # "vgg-16",
         "vgg-19",
-    ],
-    "batch_size": [
+    ]
+hyperparameters["batch_size"]= [
         128,
         1024,
-    ],
-    "lr": [
+    ]
+hyperparameters[ "lr"]=[
         0.1,
-        0.01,
-        0.001,
-        0.0001,
-    ],
-    "epochs": [2000],
-    "weight_decay": [
+        # 0.01,
+        # 0.001,
+        # 0.0001,
+    ]
+hyperparameters["epochs"]= [2000]
+hyperparameters["weight_decay"]= [
         0,
         5e-4,
-    ],
-    "optimizer": [
+    ]
+hyperparameters["optimizer"]= [
         "sgd",
         "adam",
-    ],
-    "save_every": [1]
-}
+    ]
+hyperparameters["save_every"]= [1]
 os.makedirs("sbatch", exist_ok=True)
 os.makedirs("logs", exist_ok=True)
 
@@ -51,6 +54,38 @@ from itertools import product
 
 combo = list(product(*hyperparameters.values()))
 print("Total number of jobs:", len(combo))
+
+
+combo = [
+    # (model, batch_size, lr, epochs, weight_decay, optimizer, save_every),
+    # vgg
+    ("vgg-9", 128, 0.1, 300, 0, "sgd", 5),
+    ("vgg-9", 1024, 0.1, 300, 0, "sgd",5),
+    ("vgg-9", 128, 0.1, 300, 5e-4, "sgd", 5),
+    ("vgg-9", 1024, 0.1, 300, 5e-4, "sgd",5),
+    ("vgg-9", 128, 0.1, 300, 0, "adam", 5),
+    ("vgg-9", 1024, 0.1, 300, 0, "adam",5),
+    ("vgg-9", 128, 0.1, 300, 5e-4, "adam", 5),
+    ("vgg-9", 1024, 0.1, 300, 5e-4, "adam",5),
+    
+    ("vgg-19", 128, 0.1, 300, 5e-4, "sgd",-1),
+
+    # resnet
+    ("resnet-56-noshort-1", 128, 0.1, 300, 5e-4, "sgd", 1),
+    ("resnet-56-short-1", 128, 0.1, 300, 5e-4, "sgd", 1),
+    ("resnet-56-noshort-2", 128, 0.1, 300, 5e-4, "sgd", -1),
+    ("resnet-56-short-2", 128, 0.1, 300, 5e-4, "sgd", -1),
+    ("resnet-56-noshort-4", 128, 0.1, 300, 5e-4, "sgd", -1),
+    ("resnet-56-short-4", 128, 0.1, 300, 5e-4, "sgd", -1),
+    ("resnet-56-noshort-8", 128, 0.1, 300, 5e-4, "sgd", -1),
+    ("resnet-56-short-8", 128, 0.1, 300, 5e-4, "sgd", -1),
+
+    ("resnet-20-short-1", 128, 0.1, 300, 5e-4, "sgd", 1),
+    ("resnet-20-noshort-1", 128, 0.1, 300, 5e-4, "sgd", 1),
+]
+
+if args.single_file:
+    filename = "sbatch/sbatch-singlefile.sh"
 
 for hyperparam in combo:
     argsdict = dict(zip(hyperparameters.keys(), hyperparam))
@@ -60,16 +95,22 @@ for hyperparam in combo:
     args_namespace = argparse.Namespace(**argsdict)
     unique_id = gen_unique_id(args_namespace)
     weights_path = f"weights/{unique_id}.pt"
-    filename = f"sbatch/{unique_id}.sh"
+    if not args.single_file:
+        filename = f"sbatch/{unique_id}.sh"
     logfile = f"logs/{unique_id}.out"
 
     model = argsdict["model"]
     cmds = [
+        f"# jobname: {filename}",
         f"mkdir -p logs", f"python3 mp.py {argstring}",
         f"python3 vis_mp.py --weight_path {weights_path} --model {model} --range {50}"
     ]
 
-    sbatch_file = sbatch_file.replace("###HERE###", "\n".join(cmds))
+    new_sbatch_file = sbatch_file.replace("###HERE###", "\n".join(cmds))
 
-    with open(filename, "w") as f:
-        f.write(sbatch_file)
+    if args.single_file:
+        with open(filename, "a") as f:
+            f.write(new_sbatch_file)
+    else:
+        with open(filename, "w") as f:
+            f.write(new_sbatch_file)
